96b5fcf04590b1e2eedb5cf6e096c5b76b4657ac
Robendl <robendelange@gmail.com>
2025-06-13 15:11:29 +0200

    add habrok run script

---
+++ habrok_script/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --time=0-00:30:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --mem=32GB
+#SBATCH --job-name=bus-ttp
+#SBATCH --output=bus-ttp.out
+
+module purge
+module load Python/3.13
+
+source bus-ttp/bin/activate
+
+python3 src/main.py --config-name config_habrok
+
+deactivate

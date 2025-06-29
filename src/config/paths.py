e084a474783bcd6993f92ffc74d35ce384c7d813
Robendl <robendelange@gmail.com>
2025-06-29 21:57:58 +0200

    new requirements

--- src/config/paths.py
+++ src/config/paths.py
@@ -2,6 +2,6 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-DATASETS_DIR = os.path.join(ROOT_DIR, "data/")
+DATASETS_DIR = os.path.join(ROOT_DIR, "datasets/")
 CONFIG_DIR = os.path.join(ROOT_DIR, "src/config/")
 HYDRA_OUTPUT_DIR = os.path.join(ROOT_DIR, "outputs")

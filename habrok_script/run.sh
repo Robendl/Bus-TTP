3d5900c62f7b4ef997c236734d96ff99fdefcbb8
Robendl <robendelange@gmail.com>
2025-06-30 15:21:51 +0200

    4 workers

--- habrok_script/run.sh
+++ habrok_script/run.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=3
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=64GB
 #SBATCH --job-name=bus-ttp
 #SBATCH --output=bus-ttp.out

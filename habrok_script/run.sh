416b8253ec3f7e1e84763934cc5897f11b80bddc
Robendl <robendelange@gmail.com>
2025-07-02 10:57:04 +0200

    use 6 workers

--- habrok_script/run.sh
+++ habrok_script/run.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=6
 #SBATCH --mem=64GB
 #SBATCH --job-name=bus-ttp
 #SBATCH --output=bus-ttp.out

97bbc18751a86859878da67a6d81db43d8d78455
Robendl <robendelange@gmail.com>
2025-07-23 10:19:27 +0200

    cpu run

--- habrok_script/cpu_run.sh
+++ habrok_script/cpu_run.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=0-00:30:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=20GB
+#SBATCH --mem=40GB
 #SBATCH --job-name=data-conv
 #SBATCH --output=data-conv.out
 
@@ -12,6 +12,6 @@ module load Python/3.13
 
 source ~/venvs/bus-ttp/bin/activate
 
-python3 src/main_lstm.py --config-name config_habrok pre_data_conversions=True
+python3 src/heatmap.py --config-name config_habrok
 
 deactivate

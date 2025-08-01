9846f94388a72d7febbf90e309437289bafd6905
Robendl <robendelange@gmail.com>
2025-08-01 13:39:28 +0200

    run fs file

--- habrok_script/cpu_run.sh
+++ habrok_script/cpu_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=0-00:20:00
+#SBATCH --time=0-04:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64GB
@@ -12,6 +12,6 @@ module load Python/3.13
 
 source ~/venvs/bus-ttp/bin/activate
 
-python3 src/analysis.py --config-name config_habrok
+python3 src/main_feature_selection.py --config-name config_habrok
 
 deactivate

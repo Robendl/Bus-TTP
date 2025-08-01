ac5f37a7a356256cbadc0e773b70b7ec44334275
Robendl <robendelange@gmail.com>
2025-08-01 16:22:03 +0200

    re-load data

--- src/data/data_conversions.py
+++ src/data/data_conversions.py
@@ -23,25 +23,24 @@ def iqr_filter(group, column="recorded_elapsed_time"):
     return group[(group[column] >= lower) & (group[column] <= upper)]
 
 def preprocess_splits(cfg, path):
-    # df = pd.read_parquet(path + ".parquet")
-    # print(df.shape, flush=True)
-    # df = df[df.groupby("route_seq_hash")["route_seq_hash"].transform("count") >= 4]
-    # print(df.shape, flush=True)
-    # df = df.groupby("route_seq_hash", group_keys=False).apply(iqr_filter)
-    # print(df.shape, flush=True)
-    #
-    # dataset_bundle = split_data(cfg, df)
-    # dataset_bundle = scale_time_features(cfg, dataset_bundle)
-    # dataset_bundle.save(paths.DATASET_BUNDLE_DIR)
+    df = pd.read_parquet(path + ".parquet")
+    print(df.shape, flush=True)
+    df = df[df.groupby("route_seq_hash")["route_seq_hash"].transform("count") >= 4]
+    print(df.shape, flush=True)
+    df = df.groupby("route_seq_hash", group_keys=False).apply(iqr_filter)
+    print(df.shape, flush=True)
 
-    dataset_bundle = DatasetBundle.load(paths.DATASET_BUNDLE_DIR)
+    dataset_bundle = split_data(cfg, df)
+    dataset_bundle = scale_time_features(cfg, dataset_bundle)
+    dataset_bundle.save(paths.DATASET_BUNDLE_DIR)
+
     # full_df = pd.read_csv(paths.DATASETS_DIR + cfg.dataset.metadata + ".csv")
     # val_metadata = full_df[full_df["id"].isin(dataset_bundle.val.x["id"])]
     # val_metadata.to_parquet(paths.DATASETS_DIR + cfg.dataset.metadata + "_val.parquet")
     #
-    # full_df = pd.read_csv(paths.DATASETS_DIR + cfg.dataset.geoms + ".csv")
-    # val_geoms = full_df[full_df["route_seq_hash"].isin(dataset_bundle.val.x["route_seq_hash"])]
-    # val_geoms.to_parquet(paths.DATASETS_DIR + cfg.dataset.geoms + "_val.parquet")
+    full_df = pd.read_csv(paths.DATASETS_DIR + cfg.dataset.geoms + ".csv")
+    val_geoms = full_df[full_df["route_seq_hash"].isin(dataset_bundle.val.x["route_seq_hash"])]
+    val_geoms.to_parquet(paths.DATASETS_DIR + cfg.dataset.geoms + "_val.parquet")
 
     train_hashes = set(dataset_bundle.train.x["route_seq_hash"].unique())
     return train_hashes
@@ -50,7 +49,6 @@ def create_route_dict(cfg: Config, path, train_hashes, aggregated=False):
     df = pd.read_csv(path + ".csv")
     df = scale_route_lookup(cfg, df, train_hashes)
     df.to_parquet(path + ".parquet")
-    return
     route_lookup = {}
 
     for hash_val, group in tqdm(df.groupby("route_seq_hash")):

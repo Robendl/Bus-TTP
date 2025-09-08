f0ce808f072c7c4e0cbf7f0102836a87f6cce4d7
Robendl <robendelange@gmail.com>
2025-09-08 20:52:54 +0200

    moved data to scratch

--- src/config/paths.py
+++ src/config/paths.py
@@ -1,8 +1,13 @@
 import os
+import socket
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-DATASETS_DIR = os.path.join(ROOT_DIR, "datasets/")
+if socket.gethostname().startswith('linux'):
+    DATASETS_DIR = os.path.join(ROOT_DIR, "datasets/")
+else:
+    DATASETS_DIR = os.path.join("/scratch/s3799174/datasets/")
+
 DATASET_BUNDLE_DIR = os.path.join(DATASETS_DIR, "splits/")
 RESULTS_DIR = os.path.join(ROOT_DIR, "results/")
 CONFIG_DIR = os.path.join(ROOT_DIR, "config/")

08f805d8db94c1a46b73ed61c5c695b8a21ca8a8
Robendl <robendelange@gmail.com>
2025-09-13 12:01:53 +0200

    config setup for multi

---
+++ habrok_script/mult-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --time=0-04:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=50GB
+#SBATCH --job-name=bus-ttp
+#SBATCH --output=bus-ttp.out
+
+module purge
+module load Python/3.13
+
+source ~/venvs/bus-ttp/bin/activate
+
+python3 src/multi_run.py --config-name config_multi
+
+deactivate

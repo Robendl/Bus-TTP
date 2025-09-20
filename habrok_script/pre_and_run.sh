7ca1ac746e2717a4acf5dd5e19a40577a6157f2d
Robendl <robendelange@gmail.com>
2025-09-20 21:57:58 +0200

    test run without validation

---
+++ habrok_script/pre_and_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --time=0-04:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=50GB
+#SBATCH --job-name=bus-ttp
+#SBATCH --output=bus-ttp.out
+
+module purge
+module load Python/3.13
+
+source ~/venvs/bus-ttp/bin/activate
+
+python3 src/main.py --config-name config_habrok pre_data_conversions=True
+python3 src/main.py --config-name config_habrok
+
+deactivate

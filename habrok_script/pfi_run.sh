7c96952a58903e98de062350988d86bccd462627
Robendl <robendelange@gmail.com>
2025-10-05 21:08:33 +0200

    lstm pfi run reversed

--- habrok_script/pfi_run.sh
+++ habrok_script/pfi_run.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=50GB
+#SBATCH --mem=54GB
 #SBATCH --job-name=pfi-ttp
 #SBATCH --output=pfi-ttp.out
 
@@ -14,6 +14,7 @@ module load Python/3.13
 
 source ~/venvs/bus-ttp/bin/activate
 
-python3 src/feature_importance.py --config-name config_habrok
+python3 src/main.py --config-name config_habrok pre_data_conversions=True
+python3 src/feature_importance_lstm.py --config-name config_habrok
 
 deactivate
